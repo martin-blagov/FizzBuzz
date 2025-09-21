ced8bd287abaea6be347aecf0687c5e95a0c97a1
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-09-21 19:51:50 -0400

    called doFizzBuzz method and  main method

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(100);
+        String[] newArgs = {};
+        FizzBuzz.main(newArgs);
+    }
+}
