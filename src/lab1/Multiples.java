a8f6e85cadd589e3085b3db4a1b37ef5019945b9
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-09-21 23:14:21 -0400

    generalized multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,26 @@ package lab1;
 
 public class Multiples {
     /**
-     * Count the multiples of 3 and 5 below 1000.
+     * Print out the number of multiples of 3 and 5 below 1000.
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    /**
+     * Count the multiples of a and b below n.
+     * @param a
+     * @param b
+     * @return count
+     */
+    static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 1000; i++){
-            if (i % 3 == 0 || i % 5 == 0){
+        for (int i = 1; i < n; i++){
+            if (i % a == 0 || i % b == 0){
                 count++;
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
